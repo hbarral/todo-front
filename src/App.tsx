0c3d423a39717f373104912a705cba5ab7c48f63
Héctor Barral <hbarral@mail.ru>
2022-06-04 18:11:50 -0400

    ADD eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
+import React, { FC } from 'react'
 import './App.css'
 
-function App() {
-
+export const App: FC = () => {
   return (
-    <div className="App">
-      <h1 className="text-6xl text-black">Todo App</h1>
+    <div className='App'>
+      <h1 className='text-6xl text-black'>Todo App</h1>
     </div>
   )
 }
-
-export default App
